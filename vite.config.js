a7b8b681988e63120a9851e809781b342614aea1
Sohail Ansari <sohailansarisa318@gmail.com>
2026-02-21 23:25:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-codex/add-animations-to-the-ui-038b7n
   server: {
     host: true,
     port: 4173
   }
 =======
-main
 });
